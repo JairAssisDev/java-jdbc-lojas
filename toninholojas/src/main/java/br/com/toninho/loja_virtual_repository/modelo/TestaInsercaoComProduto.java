0b75f684bd6bb50f9d2b746efcff2a807bbde310
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2023-08-09 23:59:30 -0300

    Add files via upload

--- toninholojas/src/main/java/br/com/toninho/loja_virtual_repository/modelo/TestaInsercaoComProduto.java
+++ toninholojas/src/main/java/br/com/toninho/loja_virtual_repository/modelo/TestaInsercaoComProduto.java
@@ -1,12 +1,11 @@
 package br.com.toninho.loja_virtual_repository.modelo;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 import br.com.toninho.loja_virtual_repository.ConnectionFactory;
+import br.com.toninho.loja_virtual_repository.PersistenciaProduto;
 
 public class TestaInsercaoComProduto {
 
@@ -16,21 +15,9 @@ public class TestaInsercaoComProduto {
 		Prodoto comoda = new Prodoto("comoda", "comoda vertical");
 
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-
-			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?,?)";
-
-			try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-
-				pstm.setString(1, comoda.getNome());
-				pstm.setString(2, comoda.getDescricao());
-				pstm.execute();
-				
-				try(ResultSet rst = pstm.getGeneratedKeys()){
-					while(rst.next()) {
-						comoda.setId(rst.getInt(1));
-					}
-				}
-			}
+			PersistenciaProduto persistenciaProduto = new PersistenciaProduto(connection);
+			persistenciaProduto.salvarProduto(comoda);
+			
 		}
 		System.out.println(comoda);
 	}
